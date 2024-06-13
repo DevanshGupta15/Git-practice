7d727ca9c64e5bc856a44253f8e1289585b2c75b
DevanshGupta15 <devanshguptahero@gmail.com>
2024-06-13 10:00:07 +0530

    newfeature added

--- sum.java
+++ sum.java
@@ -2,3 +2,7 @@ int x = 5;
 int y = 6;
 int sum = x + y;
 System.out.println(sum); // Print the sum of x + y
+int x = 4;
+int y = 8;
+int sum = x + y;
+System.out.println(sum); // Print the sum of x + y
